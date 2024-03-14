a09bc25486a3a73a3937c252c410426f94c60843
wachichi0917 <147352170+wachichi0917@users.noreply.github.com>
2024-03-14 14:33:05 +0900

    Update TangibleAsset.java

--- TangibleAsset.java
+++ TangibleAsset.java
@@ -1,3 +1,15 @@
-public class TangibleAsset{
-  Sysytem.out.println("abc");
+public abstract class TangibleAsset{
+  private String name;
+  private int price;
+  private String color;
+
+  public TangibleAsset(String name, int price, String color) {
+    this.name = name;
+    this.name = price;
+    this.color = color;
+  }
+  public String getName() {return this.name;}
+  public int getPrice() {return this.price;}
+  public String get color() {return this.color;}
+
 }
